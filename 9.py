f5ec370934ab51a30ec34c0dcda1bcd6d3a43632
Petr Mitin <petr09mitin@mail.ru>
2023-01-14 21:44:11 +0300

    fixed 9 in -1 case

--- 9.py
+++ 9.py
@@ -6,7 +6,10 @@ with open('9.txt') as file:
         if len(arr) == 0:
             arr.append((coord, 0))
             continue
-        arr = [elem for elem in arr if coord-elem[0] <= k]
-        min_num_before = min([elem[1] for elem in arr])
+        arr = [elem for elem in arr if (coord-elem[0] <= k) and elem[1] != -1]
+        if len(arr) != 0:
+            min_num_before = min([elem[1] for elem in arr])
+        else:
+            min_num_before = -2
         arr.append((coord, min_num_before+1))
     print(arr[-1][1])
