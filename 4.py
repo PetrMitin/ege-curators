8e2ed4a15e03c3c64b3bf328df73234b23c9b7f9
Petr Mitin <petr09mitin@mail.ru>
2023-01-14 20:22:48 +0300

    added 2, 3, 7

---
+++ 4.py
@@ -0,0 +1,10 @@
+for i in range(1, 10000):
+    st = '0' + '1'*i + '2'*i + '0'
+    while st.find('00') < 0:
+        st = st.replace('011', '20', 1)
+        st = st.replace('022', '10', 1)
+        st = st.replace('01', '220', 1)
+        st = st.replace('02', '110', 1)
+    if st.count('1') == 40 and st.count('2') > 50:
+        print(i)
+        break
